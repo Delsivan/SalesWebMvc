1c1829c3c93a8042dd2e2f4d0b82e4f6232e4197
Delsivan Nascimento Vieira <delsivannascimentovieira@gmail.com>
2021-02-17 17:20:55 -0300

    Other entities and second migration

---
+++ SalesWebMvc/Models/SalesRecord.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using SalesWebMvc.Models.enums;
+
+namespace SalesWebMvc.Models
+{
+    public class SalesRecord
+    {
+        public int Id { get; set; }
+        public DateTime Data { get; set; }
+        public double Amount { get; set; }
+        public SaleStatus Status { get; set; }
+        public Seller Seller { get; set; }
+        public DateTime Date { get; internal set; }
+
+        public SalesRecord()
+        {
+
+        }
+
+        public SalesRecord(int id, DateTime data, double amount, SaleStatus status, Seller seller)
+        {
+            Id = id;
+            Data = data;
+            Amount = amount;
+            Status = status;
+            Seller = seller;
+        }
+    }
+}
