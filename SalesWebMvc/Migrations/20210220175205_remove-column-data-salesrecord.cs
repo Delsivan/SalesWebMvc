ba3119fabca9da45c5641a58ff2d5f4fd6447205
Delsivan Nascimento Vieira <delsivannascimentovieira@gmail.com>
2021-02-20 15:28:28 -0300

    Preparing sales search navigation views

---
+++ SalesWebMvc/Migrations/20210220175205_remove-column-data-salesrecord.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SalesWebMvc.Migrations
+{
+    public partial class removecolumndatasalesrecord : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Data",
+                table: "SalesRecord");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Seller",
+                maxLength: 60,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Seller",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Seller",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 60);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Seller",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Data",
+                table: "SalesRecord",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+    }
+}
