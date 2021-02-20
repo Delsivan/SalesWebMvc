64761563d5b3ebb5de1e0c71a52a4370fb7b542e
Delsivan Nascimento Vieira <delsivannascimentovieira@gmail.com>
2021-02-20 15:32:46 -0300

    Implementing simple search

--- SalesWebMvc/Models/SalesRecord.cs
+++ SalesWebMvc/Models/SalesRecord.cs
@@ -6,21 +6,18 @@ namespace SalesWebMvc.Models
     public class SalesRecord
     {
         public int Id { get; set; }
-        public DateTime Data { get; set; }
         public double Amount { get; set; }
         public SaleStatus Status { get; set; }
         public Seller Seller { get; set; }
-        public DateTime Date { get; internal set; }
+        public DateTime Date { get; set; }
 
         public SalesRecord()
-        {
+        { }
 
-        }
-
-        public SalesRecord(int id, DateTime data, double amount, SaleStatus status, Seller seller)
+        public SalesRecord(int id, DateTime date, double amount, SaleStatus status, Seller seller)
         {
             Id = id;
-            Data = data;
+            Date = date;
             Amount = amount;
             Status = status;
             Seller = seller;
